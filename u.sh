71b05582ee5da9dc45bb8dc6005b102ec323720e
Max Base <MaxBaseCode@gmail.com>
2025-07-23 02:20:07 +0330

    save

--- u.sh
+++ u.sh
@@ -1,4 +1,15 @@
-mkdir data
-mkdir postgres
-docker compose down
-docker compose up -d
+#!/bin/bash
+
+docker --version
+docker compose version
+
+echo "Docker and Docker Compose updated."
+
+mkdir -p data
+mkdir -p postgres
+
+docker compose down --volumes --remove-orphans
+
+docker compose up -d --build
+
+echo "Docker Compose services rebuilt and started."
